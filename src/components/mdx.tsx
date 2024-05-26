cd20ca11736f29d3f928af1d4ef61c5755f22dd4
wiizzl <wiizz76.contact@gmail.com>
2024-05-26 22:18:32 +0200

    fix: added style for mdx tag

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -30,39 +30,44 @@ type Components = {
 };
 
 const components: Record<string, React.ComponentType<any>> = {
-    h1: ({ className, ...props }) => <h1 className={cn("text-3xl font-bold ", className)} {...props} />,
+    h1: ({ className, ...props }) => (
+        <h1 className={cn("scroll-m-20 text-4xl font-extrabold tracking-tight", className)} {...props} />
+    ),
     h2: ({ className, ...props }) => (
-        <h2 className={cn("border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0", className)} {...props} />
+        <h2 className={cn("scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0", className)} {...props} />
     ),
     h3: ({ className, ...props }) => (
-        <h3 className={cn("mt-8 scroll-m-20 text-2xl font-semibold tracking-tight", className)} {...props} />
+        <h3 className={cn("scroll-m-20 text-2xl font-semibold tracking-tight", className)} {...props} />
     ),
     h4: ({ className, ...props }) => (
-        <h4 className={cn("mt-8 scroll-m-20 text-xl font-semibold tracking-tight", className)} {...props} />
+        <h4 className={cn("scroll-m-20 text-xl font-semibold tracking-tight", className)} {...props} />
     ),
-    h5: ({ className, ...props }) => (
-        <h5 className={cn("mt-8 scroll-m-20 text-lg font-semibold tracking-tight", className)} {...props} />
-    ),
-    h6: ({ className, ...props }) => (
-        <h6 className={cn("mt-8 scroll-m-20 text-base font-semibold tracking-tight", className)} {...props} />
-    ),
-    a: ({ className, ...props }) => <a className={cn("font-medium underline underline-offset-4", className)} {...props} />,
+    a: ({ className, ...props }) => <a className={cn("underline underline-offset-4", className)} {...props} />,
     p: ({ className, ...props }) => <p className={cn("leading-7 [&:not(:first-child)]:mt-6", className)} {...props} />,
-    ul: ({ className, ...props }) => <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />,
+    ul: ({ className, ...props }) => <ul className={cn("my-6 ml-6 list-disc [&>li]:mt-2", className)} {...props} />,
     ol: ({ type, className, ...props }) => <ol type="1" className={cn("my-6 ml-6 list-decimal", className)} {...props} />,
-    li: ({ className, ...props }) => <li className={cn("mt-2", className)} {...props} />,
+    li: ({ className, ...props }) => <li className={className} {...props} />,
     blockquote: ({ className, cite, ...props }) => (
-        <blockquote className={cn("mt-6 border-l-2 pl-6 italic [&>*]:text-muted-foreground", className)} {...props} />
+        <blockquote className={cn("mt-6 border-l-2 pl-6 italic", className)} {...props} />
     ),
     hr: ({ ...props }) => <hr className="my-4 md:my-8" {...props} />,
     table: ({ className, ...props }: React.TableHTMLAttributes<HTMLTableElement>) => (
-        <table className={cn("border", className)} {...props} />
+        <table className={cn("w-full border", className)} {...props} />
     ),
-    tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => <tr className={cn("", className)} {...props} />,
+    tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
+        <tr className={cn("border-t", className)} {...props} />
+    ),
     td: ({ className, ...props }) => <td className={cn("px-4 py-2 border", className)} {...props} />,
-    code: ({ className, ...props }) => <code className={cn("px-3", className)} {...props} />,
+    code: ({ className, ...props }) => <code className={cn("p-3", className)} {...props} />,
     Image: ({ className, src, alt, width, height, ...props }) => (
-        <Image className={cn("rounded-md", className)} src={src} alt="Image du cours" width={width} height={height} {...props} />
+        <Image
+            className={cn("rounded-md bg-accent-foreground p-2", className)}
+            src={src}
+            alt="Image du cours"
+            width={width}
+            height={height}
+            {...props}
+        />
     ),
     Link: ({ className, href, ...props }) => <Link className="underline" href={href} {...props} />,
     Video: ({ id, ...props }) => <YouTubeEmbed videoid={id} {...props} />,
