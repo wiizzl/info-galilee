0097518e3e1dd0d00467d026c31921a171c169b8
wiizzl <wiizz76.contact0gmail.com>
2024-05-27 11:57:23 +0200

    feat: added term lesson

--- src/app/(content)/cours/[class]/[slug]/page.tsx
+++ src/app/(content)/cours/[class]/[slug]/page.tsx
@@ -17,12 +17,14 @@ export default async function MdxCours({ params }: { params: { slug: string } })
     const cours = await getCoursFromParams(params.slug);
 
     return (
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-10">
             <div className="flex flex-col gap-2">
                 <h1 className="text-5xl font-semibold">{cours.title}</h1>
                 <p className="text-muted-foreground">{cours.description}</p>
             </div>
-            <Mdx code={cours.body.code} />
+            <div>
+                <Mdx code={cours.body.code} />
+            </div>
         </div>
     );
 }
--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -2,7 +2,6 @@ import { YouTubeEmbed } from "@next/third-parties/google";
 import { useMDXComponent } from "next-contentlayer2/hooks";
 import Image from "next/image";
 import Link from "next/link";
-import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
@@ -11,30 +10,29 @@ type Components = {
     h2: React.FC<React.HTMLProps<HTMLHeadingElement>>;
     h3: React.FC<React.HTMLProps<HTMLHeadingElement>>;
     h4: React.FC<React.HTMLProps<HTMLHeadingElement>>;
-    h5: React.FC<React.HTMLProps<HTMLHeadingElement>>;
-    h6: React.FC<React.HTMLProps<HTMLHeadingElement>>;
-    a: React.FC<React.HTMLProps<HTMLAnchorElement>>;
     p: React.FC<React.HTMLProps<HTMLParagraphElement>>;
     ul: React.FC<React.HTMLProps<HTMLUListElement>>;
     ol: React.FC<React.HTMLProps<HTMLOListElement>>;
     li: React.FC<React.HTMLProps<HTMLLIElement>>;
     blockquote: React.FC<React.DetailedHTMLProps<React.BlockquoteHTMLAttributes<HTMLQuoteElement>, HTMLQuoteElement>>;
-    hr: React.FC<React.HTMLProps<HTMLHRElement>>;
     table: React.FC<React.TableHTMLAttributes<HTMLTableElement>>;
     tr: React.FC<React.HTMLAttributes<HTMLTableRowElement>>;
     td: React.FC<React.HTMLProps<HTMLTableCellElement>>;
     code: React.FC<React.HTMLProps<HTMLElement>>;
-    Image: React.FC<{ className?: string; src: string; alt: string; width: number; height: number }>;
+    Image: React.FC<{ className?: string; src: string; size: number }>;
     Link: React.FC<{ className?: string; href: string }>;
     Video: React.FC<{ id: string }>;
 };
 
-const components: Record<string, React.ComponentType<any>> = {
+const components: Components = {
     h1: ({ className, ...props }) => (
         <h1 className={cn("scroll-m-20 text-4xl font-extrabold tracking-tight", className)} {...props} />
     ),
     h2: ({ className, ...props }) => (
-        <h2 className={cn("scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0", className)} {...props} />
+        <h2
+            className={cn("croll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0", className)}
+            {...props}
+        />
     ),
     h3: ({ className, ...props }) => (
         <h3 className={cn("scroll-m-20 text-2xl font-semibold tracking-tight", className)} {...props} />
@@ -42,34 +40,42 @@ const components: Record<string, React.ComponentType<any>> = {
     h4: ({ className, ...props }) => (
         <h4 className={cn("scroll-m-20 text-xl font-semibold tracking-tight", className)} {...props} />
     ),
-    a: ({ className, ...props }) => <a className={cn("underline underline-offset-4", className)} {...props} />,
     p: ({ className, ...props }) => <p className={cn("leading-7 [&:not(:first-child)]:mt-6", className)} {...props} />,
     ul: ({ className, ...props }) => <ul className={cn("my-6 ml-6 list-disc [&>li]:mt-2", className)} {...props} />,
-    ol: ({ type, className, ...props }) => <ol type="1" className={cn("my-6 ml-6 list-decimal", className)} {...props} />,
+    ol: ({ type, className, ...props }) => (
+        <ol type="1" className={cn("my-6 ml-6 list-decimal", className)} {...props} />
+    ),
     li: ({ className, ...props }) => <li className={className} {...props} />,
     blockquote: ({ className, cite, ...props }) => (
         <blockquote className={cn("mt-6 border-l-2 pl-6 italic", className)} {...props} />
     ),
-    hr: ({ ...props }) => <hr className="my-4 md:my-8" {...props} />,
     table: ({ className, ...props }: React.TableHTMLAttributes<HTMLTableElement>) => (
-        <table className={cn("w-full border", className)} {...props} />
+        <table className={cn("w-full", className)} {...props} />
     ),
     tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
-        <tr className={cn("border-t", className)} {...props} />
+        <tr className={cn("m-0 border-t p-0", className)} {...props} />
     ),
-    td: ({ className, ...props }) => <td className={cn("px-4 py-2 border", className)} {...props} />,
+    td: ({ className, ...props }) => (
+        <td
+            className={cn(
+                "border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
+                className,
+            )}
+            {...props}
+        />
+    ),
     code: ({ className, ...props }) => <code className={cn("p-3", className)} {...props} />,
-    Image: ({ className, src, alt, width, height, ...props }) => (
+    Image: ({ className, src, size, ...props }) => (
         <Image
             className={cn("rounded-md bg-accent-foreground p-2", className)}
             src={src}
             alt="Image du cours"
-            width={width}
-            height={height}
+            width={size}
+            height={size}
             {...props}
         />
     ),
-    Link: ({ className, href, ...props }) => <Link className="underline" href={href} {...props} />,
+    Link: ({ className, href, ...props }) => <Link className="underline underline-offset-4" href={href} {...props} />,
     Video: ({ id, ...props }) => <YouTubeEmbed videoid={id} {...props} />,
 };
 
